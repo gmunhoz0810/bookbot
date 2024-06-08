6ebf51911a886e86b234816530eadb99cc8dce6c
gmunhoz0810 <gabrielmunhoz12345@gmail.com>
2024-06-07 23:21:08 -0400

    build initial text analysis program

---
+++ main.py
@@ -0,0 +1,50 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_char_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in the document\n")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+    print("--- End report ---")
+
+
+def sort_on(dict):
+    return dict["num"]
+
+
+def get_char_dict(text):
+    lower_text = text.lower()
+    char_dict = {}
+    for char in lower_text:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+
+    return char_dict
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read() 
+    
+    
+main()
